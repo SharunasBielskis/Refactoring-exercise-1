f1d2a93bba37cdb797220c90d580b6c32d7d5483
Sarunas Bielskis <sarunas.bielskis@hotmail.com>
2019-10-27 08:33:49 +0200

    Add files via upload

---
+++ RefactoredGildenRoseCsharpTests/ProgramTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace RefactoredGildenRoseCsharp
+{
+    //Test created in separate project as it is will not be deploed to user in the release version
+
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void ExecuteProgramMethodsTest()
+        {
+            #region Arange
+
+            string pathToDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string benchmarkFileName = "GoldenMaster.txt";
+            string pathToBenchMarkFile = Path.Combine(pathToDirectory, benchmarkFileName);
+            string expectedString = File.ReadAllText(pathToBenchMarkFile);
+
+            #endregion
+
+            #region Act
+
+            Program.ExecuteProgramMethods();
+            string reportFileName = "Report.txt";
+            string pathToReportFile = Path.Combine(pathToDirectory, reportFileName);
+            string actualString = File.ReadAllText(pathToReportFile);
+
+            #endregion
+
+            #region Assert
+
+            Assert.AreEqual(expectedString, actualString);
+
+            #endregion
+        }
+    }
+}
